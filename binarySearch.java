2ad9cf6a6e64a6d78ed9b44265f781872569985a
kamrulT30048 <kamrul.hasan@bjitacademy.com>
2022-11-23 15:49:04 +0600

    java file updated by kamrul

--- binarySearch.java
+++ binarySearch.java
@@ -1,7 +1,37 @@
 // Java implementation of recursive Binary Search
 class BinarySearch
 {
-    //TODO: implement binarySearch function
+    //updated by kamrul
+    int variableName = 48
+    
+    int binarySearch(int arr[], int x)
+    {
+ 	//this is a edit from kamrul
+        int l = 0, r = arr.length - 1;
+ 
+        // Checking element in whole array
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+ 
+            // Check if x is present at mid
+            if (arr[m] == x)
+                return m;
+ 
+            // If x greater, ignore left half
+            if (arr[m] < x)
+                l = m + 1;
+ 
+            // If x is smaller,
+            // element is on left side
+            // so ignore right half
+            else
+                r = m - 1;
+        }
+ 
+        // If we reach here,
+        // element is not present
+        return -1;
+    }
   
     // Driver method to test above
     public static void main(String args[])
